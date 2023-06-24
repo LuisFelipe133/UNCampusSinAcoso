3aacb57cbb17e60fa7639b8c0a9c96b7a9bd5a1e
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2023-06-24 12:57:10 -0500

    creación de algunos scripts de sql

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class DevelopmentConfig(Config):
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'root'
-    MYSQL_DB = 'us'
+    MYSQL_DB = 'bd'
 config={
     'development':DevelopmentConfig,
 }
